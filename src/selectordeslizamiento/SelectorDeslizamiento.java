e15cda5ba80db3a2955d22aca15ac741c7660490
Carlos <carlostorreslinan@gmail.es>
2021-11-28 18:58:47 +0100

    Añadimos comentatrios en selecto deslizamiento

--- src/selectordeslizamiento/SelectorDeslizamiento.java
+++ src/selectordeslizamiento/SelectorDeslizamiento.java
@@ -35,6 +35,7 @@ public class SelectorDeslizamiento extends AnchorPane {
     private boolean repetitive;
 
     public SelectorDeslizamiento() {
+        //Cargamos la vista para el FXML
         FXMLLoader fxmlLoader = new FXMLLoader(
                 getClass().getResource("selector_deslizamiento.fxml"));
         fxmlLoader.setRoot(this);
@@ -44,82 +45,113 @@ public class SelectorDeslizamiento extends AnchorPane {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+        
+        //Creamos la lista de items 
         items = new ArrayList<>();
         selectedIndex = 0;
+        //Le añadimos una acción al botón previousButton y lo lanzamos con un fireEvent
         previousButton.setOnAction((ActionEvent event) -> {
             setPrevious();
             fireEvent(event);
         });
+        //Le añadimos una acción al botón nextButton y lo lanzamos con un fireEvent
         nextButton.setOnAction((ActionEvent event) -> {
             setNext();
             fireEvent(event);
         });
     }
 
+    //Añadimos los distintos valores que se van a añadir.
     public void setItems(ArrayList<String> items) {
         this.items = items;
+        //Seleccionamos el primer valor
         selectFirst();
     }
 
+    //Actualizamos al valor previo de la lista
     public void setPrevious() {
         updateItem(selectedIndex - 1);
     }
 
+    //Actualizamos al valor siguiente de la lista
     public void setNext() {
         updateItem(selectedIndex + 1);
     }
 
+    //Seleccionamos el primer valor de la lista
     public void selectFirst() {
         updateItem(0);
     }
 
+    //Seleccionamos el último valor de la lista.
     private void selectLast() {
         updateItem(items.size() - 1);
     }
 
+    /*Método para actualizar el valor*/
     private void updateItem() {
+        //En caso de que no existan valores, se pondrá al valor a "vacio"
         if (items.isEmpty()) {
             label.setText("Vacio");
-        } else {
+        } 
+        //En caso de que no esté vacio la lista de valores
+        else {
+            //En caso de que se llegue al primer valor
             if (selectedIndex < 0) {
+                //En caso de que repetitive sea verdad (permite que al darle al botón 
+                //de anterior vaya al último valor y sea cíclico), se escoge el último valor
                 if (repetitive) {
                     selectedIndex = items.size() - 1;
-                } else {
-                    selectedIndex = 0;
+                } 
+                //En caso repetitive sea falso, selectedIndex será 0 y no permite que sea cíclico
+                else {
+                    selectFirst();
                 }
             }
+            //En caso de que el item seleccionado sea superior al tamaño de la lista
             if (selectedIndex >= items.size()) {
+                //En caso de que repetitive sea verdad (permite que al darle al botón 
+                //de siguiente vaya al primer valor y sea cíclico), se escoge el primer valor
                 if (repetitive) {
                     selectedIndex = 0;
-                } else {
-                    selectedIndex = items.size() - 1;
+                }
+                //En caso de que no sea repetitivo se pone al últimmo valor y no permite que sea cíclico.
+                else {
+                    selectLast();
                 }
             }
+            //Muestra el valor del item
             label.setText(items.get(selectedIndex));
         }
     }
-
+    
+    //Actualiza la lista de items
     private void updateItem(int selectedIndex) {
         this.selectedIndex = selectedIndex;
         updateItem();
     }
 
+    //Introduce el valor de repetitivo o no,es decir permite que sea ciclico, que desde el primero vaya al último y viceversa
     public void setRepetitive(boolean cyclesThrough) {
         this.repetitive = cyclesThrough;
     }
 
+    //Proporciona a que se pueda utilizar la acción "onAction" por parte de la app que utilice el control
     public final ObjectProperty<EventHandler<ActionEvent>> onActionProperty() {
         return onAction;
     }
 
+    //Añade la acción onAction, y pone el valor que se le añade.
     public final void setOnAction(EventHandler<ActionEvent> value) {
         onActionProperty().set(value);
     }
 
+    //Obtiene el valor del onAction
     public final EventHandler<ActionEvent> getOnAction() {
         return onActionProperty().get();
     }
 
+    //Manejador de evento para cuando se active el evento onAction, señalamos como actua
     private ObjectProperty<EventHandler<ActionEvent>> onAction = new ObjectPropertyBase<EventHandler<ActionEvent>>() {
         @Override
         protected void invalidated() {
